32c1e57e33d8567e2b314b66f369f5925d4c6b5e
Oleg Osipov <vopisso@gmail.com>
2022-04-20 22:04:38 +0300

    Homework #2 is done

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -8,8 +8,7 @@
 /*
 int GetSecondNum(int num)
 {
-    num = num / 10 % 10;
-    return num;
+    return num / 10 % 10;
 }
 
 Console.Write("Input three-digit number: ");
@@ -32,3 +31,70 @@ else
 
 //****************************************************************************************************
 
+/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+/*
+int GetThirdNum(int num)
+{
+    while(num > 999)
+    {
+        num = num / 10;
+    }
+    num = num % 10;
+    return num;
+}
+
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 99)
+{
+   Console.WriteLine(number + " -> " + GetThirdNum(number));
+}
+else if(number < -99)
+{
+    int a = -1 * number;
+    Console.WriteLine(number + " -> " + GetThirdNum(a));
+}
+else
+{
+    Console.WriteLine("There is no third digit in the number");
+}
+*/
+
+//****************************************************************************************************
+
+/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+/*
+void CheckDay(int dayNum)
+{
+    if(dayNum >= 1  && dayNum <= 5)
+    {
+        Console.WriteLine(dayNum + " -> working day");
+    }
+    else if(dayNum == 6 || dayNum == 7)
+    {
+        Console.WriteLine(dayNum + " -> day off");
+    }
+    else
+    {
+        System.Console.WriteLine("Only 7 days in a week!");
+    }
+}
+
+Console.Write("Input day number: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+CheckDay(dayNumber);
+*/
