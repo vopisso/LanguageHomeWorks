4c53f51810877407dc1a68102003c0f7a5f77941
Oleg Osipov <vopisso@gmail.com>
2022-05-23 17:40:58 +0300

    Homework #8 is done

--- Homework8/Program.cs
+++ Homework8/Program.cs
@@ -91,33 +91,68 @@ PrintMatrixInt(newMatrix);
 /*
 int FindMinSum(int[,] matrix)
 {
-    int tempSum = 0;
     int numOfRow = 1;
     int sum = 0;
-    int numberOfColumns = matrix.GetLength(1);
-    for (int i = 0,  j = 0; j < numberOfColumns; j++)
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        sum += matrix[i,j];
-    }
-
-    for (int i = 1; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < numberOfColumns; j++)
+        int tempSum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
             tempSum += matrix[i, j];
         }
-        if(tempSum < sum)
+        if(sum == 0)
+        {
+            sum = tempSum;
+        }
+        else if(tempSum < sum)
         {
             sum = tempSum;
             numOfRow = i + 1;
         }
-        tempSum = 0;
     }
     return numOfRow;
 }
 
-int[,] newMatrix = CreateMatrixInt(5, 3, 1, 5);
+int[,] newMatrix = CreateMatrixInt(7, 3, 1, 5);
 PrintMatrixInt(newMatrix);
 Console.WriteLine();
-System.Console.WriteLine(FindMinSum(newMatrix) + " row");
+System.Console.WriteLine("Row #" + FindMinSum(newMatrix) + " contains the smallest sum of elements");
 */
+
+/*
+Задача 62. Заполните спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+ 1 2  3  4
+12 13 14 5
+11 16 15 6
+10 9  8  7
+*/
+
+int[,] FillMatrixSpiral(int rows, int columns)
+{
+   int[,] matrix = new int[rows, columns];
+    int row = 0;
+    int col = 0;
+    int dx = 1;
+    int dy = 0;
+    int dirChanges = 0;
+    int visits = columns;
+    for (int i = 0; i < matrix.Length; i++)
+    {
+        matrix[row, col] = i + 1;
+        if (--visits == 0)
+        {
+            visits = columns * (dirChanges % 2) + rows * ((dirChanges + 1) % 2) - (dirChanges / 2 - 1) - 2;
+            int temp = dx;
+            dx = -dy;
+            dy = temp;
+            dirChanges++;
+        }
+        col += dx;
+        row += dy;
+    }
+    return matrix;
+}
+
+PrintMatrixInt(FillMatrixSpiral(7, 3));
